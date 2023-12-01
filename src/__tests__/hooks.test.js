de05b5b57b2a3988ea54d9752ab41bbae2937bf7
GolamMuid <golammuid@gmail.com>
2023-12-01 19:28:40 +0600

    added mocks and spies

--- src/__tests__/hooks.test.js
+++ src/__tests__/hooks.test.js
@@ -2,45 +2,47 @@ import { expect, it } from "vitest";
 import { User } from "../utils/hooks";
 
 it("should have an email property", () => {
-	const testEmail = "test@gmail.com";
+  const testEmail = "test@gmail.com";
 
-	const user = new User(testEmail);
+  const user = new User(testEmail);
 
-	expect(user).toHaveProperty("email");
+  expect(user).toHaveProperty("email");
 });
 
-it("should update email the email", () => {
-	const testEmail = "test@gmail.com";
-	const newTestEmail = "newtest@gmail.com";
+//? Concurrent test
 
-	const user = new User(testEmail);
-	user.updateEmail(newTestEmail);
+it.concurrent("should update email the email", () => {
+  const testEmail = "test@gmail.com";
+  const newTestEmail = "newtest@gmail.com";
 
-	expect(user.email).toBe(newTestEmail);
+  const user = new User(testEmail);
+  user.updateEmail(newTestEmail);
+
+  expect(user.email).toBe(newTestEmail);
 });
 
 it("should store the provided email value", () => {
-	const testEmail = "test@gmail.com";
+  const testEmail = "test@gmail.com";
 
-	const user = new User(testEmail);
+  const user = new User(testEmail);
 
-	expect(user.email).toBe(testEmail);
+  expect(user.email).toBe(testEmail);
 });
 
 it("should clear the email", () => {
-	const testEmail = "test@gmail.com";
+  const testEmail = "test@gmail.com";
 
-	const user = new User(testEmail);
-	user.clearEmail();
+  const user = new User(testEmail);
+  user.clearEmail();
 
-	expect(user.email).toBe("");
+  expect(user.email).toBe("");
 });
 
 it("should still have an email property after clearing", () => {
-	const testEmail = "test@gmail.com";
+  const testEmail = "test@gmail.com";
 
-	const user = new User(testEmail);
-	user.clearEmail();
+  const user = new User(testEmail);
+  user.clearEmail();
 
-	expect(user).toHaveProperty("email");
+  expect(user).toHaveProperty("email");
 });
